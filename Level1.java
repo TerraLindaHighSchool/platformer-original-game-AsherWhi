e7ab5652885493207730dd2d98697c78c2531bdf
AsherWhi <89489616+AsherWhi@users.noreply.github.com>
2021-11-02 14:19:04 -0700

    4

--- Level1.java
+++ Level1.java
@@ -29,10 +29,10 @@ public class Level1 extends World
     private void prepare()
     {
         setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class,
-        Door.class, HUD.class);
+            Door.class, HUD.class);
         Door door = new Door();
         addObject(door,1177,43);
-        Player player = new Player();
+        Player player = new Player(3, 5.6f, GRAVITY, 3, 3, Level2.class, MUSIC);
         addObject(player,20,524);
         addObject(new Floor(), 600, 710);
         addObject(new BrickWall(), 985, 104);
